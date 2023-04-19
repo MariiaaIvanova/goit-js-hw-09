6c341e0c4764df19047d09bc2f8adf463e343034
MariiaaIvanova <114428702+MariiaaIvanova@users.noreply.github.com>
2023-04-19 17:14:23 +0000

    Deploying to gh-pages from @ MariiaaIvanova/goit-js-hw-091@c5e807df53acf830db0cbc41d93e26c5f26eaaf3 🚀

---
+++ 01-color-switcher.3a8a6c5c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,o=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));t.style.backgroundColor=e}),1e3)})),n.addEventListener("click",(function(){n.disabled=!0,e.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.3a8a6c5c.js.map
